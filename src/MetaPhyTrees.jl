a336ac7557fb33d18611a5cd0a5700c12a478925
banhbio <ban@kuicr.kyoto-u.ac.jp>
2023-01-11 14:13:09 +0900

    export functions

--- src/MetaPhyTrees.jl
+++ src/MetaPhyTrees.jl
@@ -8,8 +8,25 @@ using AxisArrays
 include("types.jl")
 include("indexing.jl")
 include("trees.jl")
+include("distance.jl")
 include("show.jl")
 
+
+#export type utils
+export isrooted, isrerootable
+
+#export basic funtions
+export rootindex, parent_edge, leafedges, leaves, isleaf, isinternal,
+       reindex!, reroot!, swapchildren!, swap!, ladderize!,
+       add_child!, rem_descendant!,
+       ancestors, common_ancestor,
+       print_tree, treesize, treebreadth, treeheight
+
+#export distance functions
+export distance, distance_matrix, treelength
+
 include("newick/newick.jl")
+import ..Newick: parse_newick
+export Newick, parse_newick
 
 end
