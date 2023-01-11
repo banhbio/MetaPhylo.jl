3fceee55254490368d13bdaa33288745f0c8aeb5
banhbio <ban@kuicr.kyoto-u.ac.jp>
2023-01-11 11:48:42 +0900

    add leaves/internal utils

--- src/trees.jl
+++ src/trees.jl
@@ -14,3 +14,51 @@ Base.setindex!(tree::Tree, data, idx1::Integer, idx2::Integer) = setindex!(tree.
 
 Base.getindex(tree::Tree, idx::Integer, ::Colon) = IndexNode(tree, idx)
 Base.getindex(tree::Tree, ::Colon) = IndexNode(tree)
+
+"""
+    parent_edge(tree::Tree, idx::Integer)
+Return the edge between the specified `idx` node and its parent node. If the node is root, this returns `nothing` 
+"""
+function parent_edge(tree::Tree{Code}, idx::Integer) where {Code}
+    pidx = parentindex(tree, idx)
+    return isnothing(pidx) ? nothing : Edge{Code}(pidx, idx)
+end
+
+"""
+    leaves(tree::Tree, [idx::Integer])
+Return the indices of all leaves in the `tree`. If the index is specified, this returns the indices of leaves in its subtree.
+"""
+leaves(tree::Tree, idx::Integer) = nodevalue.(Leaves(IndexNode(tree, idx)))
+leaves(tree::Tree) = leaves(tree, rootindex(tree))
+
+"""
+    leafedges(tree::Tree, [idx::Integer])
+Return the edges of all edges connected to the leaves in the `tree`. If the index is specified, this returns the edges connected to its leaves in the `tree`.
+"""
+leafedges(tree::Tree{Code}, idx::Integer) where {Code} = Vector{Edge{Code}}([parent_edge(tree, lf) for lf in leaves(tree, idx) if rootindex(tree) != lf])
+leafedges(tree::Tree) = leafedges(tree, rootindex(tree))
+
+"""
+    isleaf(tree::Tree, idx::Integer)
+Return `true` if the `idx` is contained in a leaf node of the `tree`.
+"""
+isleaf(tree::Tree, idx::Integer) = haskey(tree, idx) && idx in leaves(tree)
+
+"""
+    isleaf(tree::Tree, edge::Edge)
+Return `true` if the `edge` is connected to a leaf node of the `tree`.
+"""
+isleaf(tree::Tree, edge::Edge) = haskey(tree, edge) && edge in leafedges(tree)
+
+"""
+    isinternal(tree::Tree, idx::Integer)
+Return `true` if the `idx` is contained in an internal node of the `tree`.
+"""
+isinternal(tree::Tree, idx::Integer) = haskey(tree, idx) && !isleaf(tree, idx)
+
+"""
+    isinternal(tree::Tree, idx::Integer)
+Return `true` if the `edge` is both connected to internal nodes of the `tree`.
+"""
+isinternal(tree::Tree, edge::Edge) = haskey(tree, edge) && !isleaf(tree, edge)
+
