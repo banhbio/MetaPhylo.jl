69b36897f729933815ab79f0d8d757c72e7ea0aa
Documenter.jl <documenter@juliadocs.github.io>
2023-01-11 02:34:51 +0000

    build based on 255314f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MetaPhyTrees","category":"page"},{"location":"#MetaPhyTrees","page":"Home","title":"MetaPhyTrees","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MetaPhyTrees.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MetaPhyTrees]","category":"page"}]
+}
