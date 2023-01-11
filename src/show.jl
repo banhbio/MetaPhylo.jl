eaa6550c0256483a0ba1793858dfc23091962836
banhbio <ban@kuicr.kyoto-u.ac.jp>
2023-01-11 13:44:09 +0900

    add show

---
+++ src/show.jl
@@ -0,0 +1,33 @@
+function Base.show(io::IO, tree::Tree{Code, rooted, rerootable})
+    print(
+        io,
+        """
+        MetaPhylo.Tree with $(length(leaves(tree))) leaves.
+        Rooted: $(rooted)
+        Rerootable: $(rerootable)
+        """
+    )
+    nothing
+end
+AbstractTrees.print_tree(tree::Tree; kwargs...) = print_tree(IndexNode(tree); kwargs...)
+AbstractTrees.print_tree(io::IO, tree::Tree; kwargs...) = print_tree(io, IndexNode(tree); kwargs...)
+AbstractTrees.print_tree(f::Function, io::IO, tree::Tree; kwargs...) = print_tree(f::Function, io, IndexNode(tree); kwargs...)
+
+function _join_namedtuplevalues(dict::Dict{Symbol, Any}, delim::AbstractString)
+    entries = map(collect(dict)) do (key,val)
+        keystr = String(key)
+        valuestr = sprint(show, val)
+        keystr * ":" * valuestr
+    end
+    return join(entries, delim)
+end
+
+function AbstractTrees.printnode(io::IO, idxnode::IndexNode)
+    tree = idxnode.tree
+    node = idxnode.index
+    pnode = parentindex(tree, node)
+    branch = isnothing(pnode) ? nothing : Edge(pnode, node)
+    nodestr = _join_namedtuplevalues(tree[node], ", ")
+    branchstr = isnothing(branch) ? "root" : _join_namedtuplevalues(tree[branch], ", ")
+    print(IOContext(io, :compact => true, :limit => true), "$node: [$branchstr] $nodestr")
+end
