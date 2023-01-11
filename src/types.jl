f93a7a7a82b469f67577d0e3305feb18825c3737
banhbio <ban@kuicr.kyoto-u.ac.jp>
2023-01-11 13:43:57 +0900

    fix type params

--- src/types.jl
+++ src/types.jl
@@ -1,14 +1,25 @@
 import Base: ==
 
-mutable struct Tree{Code<:Integer, rooted, rerootable}
+mutable struct Tree{Code<:Integer, <:RootState, <:ReRootablilty}
     graph::DiGraph{Code}
     root::Code
     node_data::Dict{Code, Dict{Symbol,Any}}
     branch_data::Dictionary{Edge{Code}, Dict{Symbol, Any}}
 end
 
-isrooted(::Type{<:Tree{Code, rooted, rerootable}}) where {Code, rooted, rerootable} = rooted::Bool
+abstract type RootState end
+
+struct Rooted <: RootState end
+struct UnRooted <: RootState end
+
+isrooted(::Type{<:Tree{Code, Rooted}}) where {Code} = true
+isrooted(::Type{<:Tree{Code, UnRooted}}) where {Code} = false
 isrooted(tree::Tree) = isrooted(typeof(tree))
 
-isrerootable(::Type{<:Tree{Code, rooted, rerootable}}) where {Code, rooted, rerootable} = rerootable::Bool
+abstract type ReRootablilty end
+struct ReRootable <: ReRootablilty end
+struct NotReRootable <: ReRootablilty end
+
+isrerootable(::Type{<:Tree{Code, rooted, ReRootable}}) where {Code, rooted} = true
+isrerootable(::Type{<:Tree{Code, rooted, NotReRootable}}) where {Code, rooted} = false
 isrerootable(tree::Tree) = isrerootable(typeof(tree))
