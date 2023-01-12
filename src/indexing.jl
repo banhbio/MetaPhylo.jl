d3c85471286ff194fcfdf076882b990c0898caf4
banhbio <ban@kuicr.kyoto-u.ac.jp>
2023-01-12 23:48:30 +0900

    fix indexing

--- src/indexing.jl
+++ src/indexing.jl
@@ -1,20 +1,20 @@
-AbstractTrees.nodevalue(::Tree, idx::T) where T<:Integer = idx
+AbstractTrees.nodevalue(::AbstractPhyloTree, idx::T) where T<:Integer = idx
 
-AbstractTrees.ParentLinks(::Type{<:Tree}) = StoredParents()
-AbstractTrees.ChildIndexing(::Type{<:Tree}) = IndexedChildren()
-AbstractTrees.NodeType(::Type{<:Tree}) = HasNodeType()
-AbstractTrees.nodetype(::Type{<:Tree{T}}) where T = T
-AbstractTrees.NodeType(::Type{<:IndexNode{N,T}}) where {N<:Tree,T} = HasNodeType()
-AbstractTrees.nodetype(::Type{<:IndexNode{N,T}}) where {N<:Tree,T} = IndexNode{N,T}
-AbstractTrees.SiblingLinks(::Type{<:Tree}) = StoredSiblings()
+AbstractTrees.ParentLinks(::Type{<:AbstractPhyloTree}) = StoredParents()
+AbstractTrees.ChildIndexing(::Type{<:AbstractPhyloTree}) = IndexedChildren()
+AbstractTrees.NodeType(::Type{<:AbstractPhyloTree}) = HasNodeType()
+AbstractTrees.nodetype(::Type{<:AbstractPhyloTree{T}}) where T = T
+AbstractTrees.NodeType(::Type{<:IndexNode{N,T}}) where {N<:AbstractPhyloTree,T} = HasNodeType()
+AbstractTrees.nodetype(::Type{<:IndexNode{N,T}}) where {N<:AbstractPhyloTree,T} = IndexNode{N,T}
+AbstractTrees.SiblingLinks(::Type{<:AbstractPhyloTree}) = StoredSiblings()
 
-AbstractTrees.childindices(tree::Tree, idx) = outneighbors(tree.graph, idx)
-function AbstractTrees.parentindex(tree::Tree, idx)
+AbstractTrees.childindices(tree::AbstractPhyloTree, idx) = outneighbors(tree.graph, idx)
+function AbstractTrees.parentindex(tree::AbstractPhyloTree, idx)
     inne = inneighbors(tree.graph, idx)
     isempty(inne) ? nothing : first(inne)
 end
 
-function AbstractTrees.nextsiblingindex(tree::Tree, idx)
+function AbstractTrees.nextsiblingindex(tree::AbstractPhyloTree, idx)
     pidx = parentindex(tree, idx)
     isnothing(pidx) && return nothing
     sibilings = outneighbors(tree.graph, pidx)
@@ -22,7 +22,7 @@ function AbstractTrees.nextsiblingindex(tree::Tree, idx)
     return pos == length(sibilings) ? nothing : sibilings[pos+1]
 end
 
-function AbstractTrees.prevsiblingindex(tree::Tree, idx)
+function AbstractTrees.prevsiblingindex(tree::AbstractPhyloTree, idx)
     pidx = parentindex(tree, idx)
     isnothing(pidx) && return nothing
     sibilings = outneighbors(tree.graph, pidx)
@@ -30,4 +30,4 @@ function AbstractTrees.prevsiblingindex(tree::Tree, idx)
     return pos == 1 ? nothing : sibilings[pos-1]
 end
 
-AbstractTrees.rootindex(tree::Tree) = tree.root
+AbstractTrees.rootindex(tree::AbstractPhyloTree) = tree.root
