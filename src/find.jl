64488b120c5b249205e6a6b10f6291416bc53e04
banhbio <ban@kuicr.kyoto-u.ac.jp>
2023-01-16 23:12:12 +0900

    support multi-key function

--- src/find.jl
+++ src/find.jl
@@ -1,15 +1,21 @@
+OneOrVector{T} = Union{T, Vector{T}}
+
 """
     findnodes(tree::AbstractPhyloTree, args...; ifnot_haskey::Bool=false)
 Return indices of tree node which all values proceed by transformation(s) `args` for a given attribute are true.
 """
-function findnodes(tree::AbstractPhyloTree, args::Pair{Symbol, <:Base.Callable}...; ifnot_haskey::Bool = false)
-    [ idx for (idx, value) in tree.node_data if all([(haskey(value, key) ? f(value[key]) : ifnot_haskey) for (key, f) in args])]
+function findnodes(tree::AbstractPhyloTree, args::Pair{<:OneOrVector{Symbol}, <:Base.Callable}...; kwargs...)
+    _find_key(tree.node_data, args...; kwargs...)
 end
 
 """
     findbranches(tree::AbstractPhyloTree, args...; ifnot_haskey::Bool=false)
 Return indices of tree branch which all values proceed by transformation(s) `args` for a given attribute are true.
 """
-function findbranches(tree::AbstractPhyloTree, args::Pair{Symbol, <:Base.Callable}...; ifnot_haskey::Bool = false)
-    [ idx for (idx, value) in tree.branch_data if all([(haskey(value, key) ? f(value[key]) : ifnot_haskey) for (key, f) in args])]
+function findbranches(tree::AbstractPhyloTree, args::Pair{<:OneOrVector{Symbol}, <:Base.Callable}...; kwargs...)
+    _find_key(tree.branch_data, args...; kwargs...)
+end
+
+function _find_key(dict::Dict, args...; ifnot_haskey=false)
+    [ idx for (idx, value) in dict if all([(all(haskey.(Ref(value), key)) ? f(getindex.(Ref(value), key)...) : ifnot_haskey) for (key, f) in args])]
 end
